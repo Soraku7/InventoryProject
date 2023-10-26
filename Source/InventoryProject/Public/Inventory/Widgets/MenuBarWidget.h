e295dc3ba0a32a4d408c0962363b4ebc82e0f5fe
Soraku7 <2673140287@qq.com>
2023-10-26 17:58:20 +0800

    MenuBar

---
+++ Source/InventoryProject/Public/Inventory/Widgets/MenuBarWidget.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "MenuBarWidget.generated.h"
+
+/** 
+ * 
+ */
+UCLASS()
+class INVENTORYPROJECT_API UMenuBarWidget : public UUserWidget
+{
+	GENERATED_BODY()
+};
